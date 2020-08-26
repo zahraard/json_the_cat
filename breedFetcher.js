f27573bd4896d0083dc13f8634e501d0c8b0a81e
Zahra Ardalani <zahra.ardalani@gmail.com>
2020-08-26 22:37:17 +0000

    cat search

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,7 +2,7 @@ const request = require('request');
 const args = process.argv;
 const breed = args.slice(2).join(' ');
 
-const resource = `https://api.thecatpi.com/v1/breeds/search?q=${breed}`;
+const resource = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
 request(resource, (error, response, body) => {
   if (error) {
     console.log(error.message);
